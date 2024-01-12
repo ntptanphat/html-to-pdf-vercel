174dde992aa657f128994f273b6f1754f95eadae
Phat Nguyen <phatnguyen@aeyes.vn>
2024-01-12 11:51:59 +0700

    Commit code

--- service.js
+++ service.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const fs = require("fs");
-const chrome = require("chrome-aws-lambda");
-const puppeteer = require("puppeteer-core");
+// const chrome = require("chrome-aws-lambda");
+// const puppeteer = require("puppeteer-core");
 
 exports.hello_world = async (req, res) => res.send('Hello World 2');
 exports.test_post = async (req, res) => res.send('Test Post');
